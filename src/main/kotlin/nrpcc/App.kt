bfdeec7821c592465e36452533d39ed358e70ded
Mark Zhitomirski <mz@exactpro.com>
2020-01-22 20:09:11 +0300

    handle node down error

--- src/main/kotlin/nrpcc/App.kt
+++ src/main/kotlin/nrpcc/App.kt
@@ -1,8 +1,19 @@
 package nrpcc
 
 fun main(args: Array<String>) {
-    require(args.isNotEmpty()) { "Usage: <binary> [exec|execw|execm] <flow-name> <flow-args>\n" +
+    require(args.isNotEmpty()) { "Usage: <binary> <cmd>\n" +
             "Example: <binary> exec MyFlow arg1: 10, toParty: PartyB" }
     val cmd = Commands.valueOf(args[0])
-    RpcCmdr("localhost:10002", "userN", "X").cmd(cmd)
+    val hostPort = "localhost:10002"
+    val user = "userN"
+    val password = "X"
+    try {
+        RpcCmdr(hostPort, user, password).cmd(cmd)
+    } catch (e: net.corda.client.rpc.RPCException) {
+        if (e.message.toString().startsWith("Cannot connect to server(s). Tried with all available servers.")) {
+            println("Cannot connect to $hostPort")
+        } else {
+            println("${e.message}")
+        }
+    }
 }
