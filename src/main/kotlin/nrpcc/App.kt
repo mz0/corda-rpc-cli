eac844f1d4150ec41c2fdd7322969d92b604d000
Mark Zhitomirski <mz@exactpro.com>
2020-01-28 12:03:05 +0300

    try FixedThreadPool

--- src/main/kotlin/nrpcc/App.kt
+++ src/main/kotlin/nrpcc/App.kt
@@ -1,5 +1,10 @@
 package nrpcc
 
+//import kotlin.concurrent.thread
+//import kotlinx.coroutines.*
+//import kotlinx.coroutines.experimental.newSingleThreadContext
+import java.util.concurrent.Executors
+
 fun main(args: Array<String>) {
     val cmd: Command
     var flowNam: String? = null
@@ -10,12 +15,27 @@ fun main(args: Array<String>) {
         else -> {cmd = Command.valueOf(args[0]); flowNam=args[1]; flowArgs=args.drop(2).joinToString(separator=" ")}
     }
 
-    val hostPort1 = "localhost:10002"
+    val n1 = "localhost:10002"
+    val n2 = "localhost:10012"
+    val n3 = "localhost:10022"
+    val n4 = "localhost:10032"
+    val n5 = "localhost:10042"
+
+    val nodes = listOf(n1, n2, n3, n4, n5)
     val user = "userN"
     val password = "X"
-    val r = RpcCmdr(hostPort1, user, password).cmd(cmd, flowNam, flowArgs)
-    Thread.sleep(12) // TODO hide this waiting in dispose(), better yet make it a parameter
-    r.dispose()
+    val numCores = 5
+
+    val runners = Executors.newFixedThreadPool(numCores)
+    nodes.forEach {
+        runners.execute() {
+            println("${System.currentTimeMillis()} $it ${Thread.currentThread().name}")
+            RpcCmdr(it, user, password).cmd(cmd, flowNam, flowArgs).dispose()
+            //Thread.sleep(12) // TODO hide this waiting in dispose(), better yet make it a parameter
+        }
+    }
+    println("${System.currentTimeMillis()} setup complete")
+    runners.shutdown()
 }
 
 fun usage() { println("Usage: <binary> <cmd>\n" +
